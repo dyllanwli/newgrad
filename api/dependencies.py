678db0fce5ffc6072c896422ce2f71f628a94ad6
dyllanwli <messsking@gmail.com>
2024-09-17 01:16:06 -0700

    update

--- api/dependencies.py
+++ api/dependencies.py
@@ -3,7 +3,6 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from api.config import MONGODB_URL
 
 client = AsyncIOMotorClient(MONGODB_URL)
-print(MONGODB_URL)
 newgrad_db = client["newgrad"]
 
 
